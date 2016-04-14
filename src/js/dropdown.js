789ed0ed7ea9a60a7547e84bccfe0215461b2749
tony-luisi <t.luisi@gmail.com>
2016-04-15 11:03:42 +1200

    done changes

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -3,16 +3,17 @@ var correctAnswer = 0
 
 //takes in the whole cohort array, and an int on the correct index of the answer
 //populates the dropdown box with 4 random answers
+//dropdown box is currently #dropbox element
 function populate(answersArray, correct){
   console.log(answersArray)
   correctAnswer = correct
-  document.querySelector('#dropbox').innerHTML = ""
-  answersArray.forEach(function(answer, i){
-    var option = document.createElement('option')
-    option.innerHTML = answer
-    option.value = i
-    document.querySelector('#dropbox').appendChild(option)
-  })
+  // document.querySelector('#dropbox').innerHTML = ""
+  // answersArray.forEach(function(answer, i){
+  //   var option = document.createElement('option')
+  //   option.innerHTML = answer
+  //   option.value = i
+  //   document.querySelector('#dropbox').appendChild(option)
+  // })
 
 
 }
