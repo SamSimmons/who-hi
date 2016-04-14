93ac304db1adfc2c5c99e78c5f61781ed4834990
tony-luisi <t.luisi@gmail.com>
2016-04-14 20:35:56 +1200

    added dropbox

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,9 +1,18 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var answers = []
+var correctAnswer = 0
 
 //takes in an array of strings with the possible answers, and an int on the correct index of the answer
 //populates the dropdown box with the answers
 function populate(answersArray, correct){
+  correctAnswer = correct
+  document.querySelector('#dropbox').innerHTML = ""
+  answersArray.forEach(function(answer, i){
+    var option = document.createElement('option')
+    option.innerHTML = answer
+    option.value = i
+    document.querySelector('#dropbox').appendChild(option)
+  })
 
 }
 
--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,8 +1,17 @@
 var answers = []
+var correctAnswer = 0
 
 //takes in an array of strings with the possible answers, and an int on the correct index of the answer
 //populates the dropdown box with the answers
 function populate(answersArray, correct){
+  correctAnswer = correct
+  document.querySelector('#dropbox').innerHTML = ""
+  answersArray.forEach(function(answer, i){
+    var option = document.createElement('option')
+    option.innerHTML = answer
+    option.value = i
+    document.querySelector('#dropbox').appendChild(option)
+  })
 
 }
 
