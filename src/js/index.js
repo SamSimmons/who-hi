dfe9f8ce70bdaa195665c1080dd144a1247e7139
sam <samuel.j.simmons@gmail.com>
2016-04-14 19:27:48 +1200

    add layout structure

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,14 @@
 var game = require('./game.js')
-game.startGame() //starts the game
+// game.startGame() //starts the game
 
-console.log("hello there")
+// console.log("hello there")
 
-var main = document.querySelector('.gameContainer')
-var imageArray = [{ name: 'harry', image: 'http://i.imgur.com/sVLVL5z.jpg'}, { name: 'polly', image: 'http://i.imgur.com/skyvLsc.png' }, { name: 'roger', image: 'http://i.imgur.com/49gsA5P.jpg' }]
+// var main = document.querySelector('.gameContainer')
+// var imageArray = [{ name: 'harry', image: 'http://i.imgur.com/sVLVL5z.jpg'}, { name: 'polly', image: 'http://i.imgur.com/skyvLsc.png' }, { name: 'roger', image: 'http://i.imgur.com/49gsA5P.jpg' }]
 
-imageArray.map(function(imageObject){
-  var imageDiv = document.createElement('div')
-  imageDiv.id = imageObject.name
-  imageDiv.style.backgroundImage = 'url(' + imageObject.image + ')'
-  main.appendChild(imageDiv)
-})
+// imageArray.map(function(imageObject){
+//   var imageDiv = document.createElement('div')
+//   imageDiv.id = imageObject.name
+//   imageDiv.style.backgroundImage = 'url(' + imageObject.image + ')'
+//   main.appendChild(imageDiv)
+// })
