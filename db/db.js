d98209ab37891a760f11382558cc9dbe378e7c88
vicken <vicken.liu@gmail.com>
2016-04-14 20:03:35 +1200

    set back to development db for frontend

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@
 var Knex = require('knex');
 var knexConfig = require('../knexfile')
 
-var knex = Knex(knexConfig[process.env.NODE_ENV || 'test'])
+var knex = Knex(knexConfig[process.env.NODE_ENV || 'development'])
 
 module.exports =  {
 
