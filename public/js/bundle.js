1965074ba8deb6e05715ce87a737afa3b35ed915
sam <samuel.j.simmons@gmail.com>
2016-04-15 00:39:34 +1200

    add high score container

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -158,6 +158,59 @@ module.exports = {
 }
 
 },{}],5:[function(require,module,exports){
+var hiScore = [
+{
+id: 14,
+name: "Adele",
+score: 22110
+},
+{
+id: 13,
+name: "Bibel",
+score: 13432
+},
+{
+id: 15,
+name: "Michal",
+score: 11313
+},
+{
+id: 11,
+name: "Silly",
+score: 2342
+},
+{
+id: 12,
+name: "New York",
+score: 2145
+},
+{
+id: 6,
+name: "Cat",
+score: 762
+},
+{
+id: 7,
+name: "meow",
+score: 231
+},
+{
+id: 9,
+name: "Data",
+score: 220
+},
+{
+id: 2,
+name: "meow",
+score: 213
+},
+{
+id: 4,
+name: "dkffdjd",
+score: 210
+}
+]
+
 var landing = function () {
 	var overlay = document.createElement('div')
 	overlay.className = 'overlay'
@@ -178,7 +231,31 @@ var landing = function () {
 	document.body.appendChild(overlay)	
 }
 
+var addHighScore = function () {
+	var hiScoreContainer = document.createElement('div')
+
+	hiScore.forEach((score, i) => {
+		var scoreElt = document.createElement('div')
+		scoreElt.className = 'score score-' + i
+
+		var nameElt = document.createElement('p')
+		nameElt.innerHTML = score.name
+		nameElt.className = "score-name"
+
+		var scoreVal = document.createElement('p')
+		scoreVal.innerHTML = score.score
+		scoreVal.className = "score-val"
+
+		scoreElt.appendChild(nameElt)
+		scoreElt.appendChild(scoreVal)
+		hiScoreContainer.appendChild(scoreElt)
+	})
+
+	document.body.appendChild(hiScoreContainer)
+}
+
 landing()
+addHighScore()
 },{}],6:[function(require,module,exports){
 var clock = $('.timer').FlipClock(30, {
   autoStart: false,
--- src/js/render.js
+++ src/js/render.js
@@ -1,3 +1,56 @@
+var hiScore = [
+{
+id: 14,
+name: "Adele",
+score: 22110
+},
+{
+id: 13,
+name: "Bibel",
+score: 13432
+},
+{
+id: 15,
+name: "Michal",
+score: 11313
+},
+{
+id: 11,
+name: "Silly",
+score: 2342
+},
+{
+id: 12,
+name: "New York",
+score: 2145
+},
+{
+id: 6,
+name: "Cat",
+score: 762
+},
+{
+id: 7,
+name: "meow",
+score: 231
+},
+{
+id: 9,
+name: "Data",
+score: 220
+},
+{
+id: 2,
+name: "meow",
+score: 213
+},
+{
+id: 4,
+name: "dkffdjd",
+score: 210
+}
+]
+
 var landing = function () {
 	var overlay = document.createElement('div')
 	overlay.className = 'overlay'
@@ -18,4 +71,28 @@ var landing = function () {
 	document.body.appendChild(overlay)	
 }
 
+var addHighScore = function () {
+	var hiScoreContainer = document.createElement('div')
+
+	hiScore.forEach((score, i) => {
+		var scoreElt = document.createElement('div')
+		scoreElt.className = 'score score-' + i
+
+		var nameElt = document.createElement('p')
+		nameElt.innerHTML = score.name
+		nameElt.className = "score-name"
+
+		var scoreVal = document.createElement('p')
+		scoreVal.innerHTML = score.score
+		scoreVal.className = "score-val"
+
+		scoreElt.appendChild(nameElt)
+		scoreElt.appendChild(scoreVal)
+		hiScoreContainer.appendChild(scoreElt)
+	})
+
+	document.body.appendChild(hiScoreContainer)
+}
+
 landing()
+addHighScore()
