0ced9a7860b20e47c0da9b03aa258c3392b599b7
oredds <oliver.jk.redding@gmail.com>
2016-04-14 17:30:41 +1200

    server

--- db/db.js
+++ db/db.js
@@ -4,24 +4,6 @@ var knexConfig = require('../knexfile')
 
 var knex = Knex(knexConfig[process.env.NODE_ENV || 'development'])
 
-<<<<<<< HEAD
-module.exports = {
-  getAll: function (tableName) {
-    return knex.select().table( tableName )
-  },
-
-  findOne: function (tableName, params) {
-    return knex(tableName).where('id',params.id)
-  },
-
-  delete: function (tableName, params) {
-    return knex(tableName).where('id',params.id).del()
-  },
-
-  update: function (tableName, searchParams, updateInfo) {
-    return knex(tableName).where('id',searchParams.id).update(updateInfo)
-  }
-=======
 module.exports =  {
 
     getAll: function (tableName) {
@@ -37,5 +19,4 @@ module.exports =  {
     add: function (tableName,newEntry) {
       return knex(tableName).insert(newEntry)
     }
->>>>>>> 081a4cc91e24b94cdb00ca30046cef39b99c5e19
 }
